6dd1abb0bccb893208727531332ec7b7ebb7fe25
Mateusz Kopij <sagasu@gmail.com>
2020-07-22 10:54:19 +0100

    introducing fastai and simple plotting

---
+++ fastai_start.py
@@ -0,0 +1,13 @@
+from fastai import *
+from fastai.vision import *
+import matplotlib.pyplot as plt
+
+from PIL import Image
+
+a = 1
+b = a + 1
+c = b + a +1
+d = c + b + a + 1
+
+plt.plot([a,b,c,d])
+plt.show()
